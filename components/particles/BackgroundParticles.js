78da50e96946e53703394497b22e3c960c27df40
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-07-23 22:48:48 +0300

    add: react-particles initialized

---
+++ components/particles/BackgroundParticles.js
@@ -0,0 +1,27 @@
+import React, { useCallback } from "react";
+import Particles from "react-particles";
+import particlesConfig from "./config";
+import { loadSlim } from "tsparticles-slim";
+export default function BackgroundParticles() {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    //await loadFull(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={particlesConfig}
+    />
+  );
+}
