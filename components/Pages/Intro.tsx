47ceae8d45da0c479938278d63d6c94239decb68
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-08-21 12:18:22 +0300

    fix: font size and text position in mobile

--- components/Pages/Intro.tsx
+++ components/Pages/Intro.tsx
@@ -3,29 +3,41 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import TechIcons from '../TechIcons'
 export default function Intro() {
+
+    const container = {
+        hidden: { opacity: 0 },
+        show: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.3
+            }
+        }
+    }
+    const item = {
+        hidden: { opacity: 0, y: 50 },
+        show: { opacity: 1, y: 0 }
+    }
     return (
-        <div className='h-full w-3/4 max-sm:mx-auto sm:w-full flex flex-col items-center justify-center text-center my-auto'>
+        <motion.div
+            initial="hidden"
+            animate="show"
+            variants={container}
+            className='h-full w-3/4 max-sm:mx-auto sm:w-full flex flex-col items-center justify-center text-center max-md:pt-[20%] mb-auto md:my-auto'>
             <motion.h1
-                initial={{ x: -100, opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-                transition={{ duration: .3, delay: .3 }}
-                className='text-4xl sm:text-6xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-red-700 to-purple-900 bg-300% animate-flow-gradient'>
+                variants={item}
+                className='text-4xl md:text-6xl xl:text-8xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-red-700 to-purple-900 bg-300% animate-flow-gradient'>
                 {"Hello World, It's Furkan!"}
             </motion.h1>
             <motion.h2
-                initial={{ x: 100, opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-                transition={{ duration: .3, delay: .6 }}
-                className='text-xl sm:text-2xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-red-700 to-purple-900 bg-300% animate-flow-gradient mb-20'>
+                variants={item}
+                className='text-2xl md:text-3xl xl:text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-red-700 to-purple-900 bg-300% animate-flow-gradient mb-20'>
                 {"I'm a fullstack web developer."}
             </motion.h2>
             <motion.div
-                initial={{ y: 100, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: .3, delay: .9 }}
+                variants={item}
                 className='h-[50px] grid grid-cols-3 sm:flex gap-2'>
                 <TechIcons />
             </motion.div>
-        </div>
+        </motion.div>
     )
 }
