a718e26a1548f6976a7d5452296bed0e357feaf1
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-08-24 17:54:28 +0300

    add: dropdown navigation menu drop animation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -43,7 +43,7 @@ export default function Navbar({ setTheme, theme, setParticles, particles, path 
     }
     return (
         <motion.header
-            className='md:shadow-sm dark:shadow-muted'>
+            className='md:shadow-sm dark:shadow-muted overflow-hidden'>
             <nav className='p-2 relative'>
                 <motion.ul
                     variants={container}
@@ -58,8 +58,77 @@ export default function Navbar({ setTheme, theme, setParticles, particles, path 
                     <motion.div
                         variants={container}
                         initial='hide'
+                        animate={isOpen ? 'show' : 'hide'}
+                        transition={{ duration: 0.3 }}
+                        className={`flex md:hidden max-md:mt-4 flex-col ${isOpen ? 'md:flex-row text-center' : 'hidden'} justify-center gap-2 z-50`}>
+                        <motion.li
+                            variants={item}
+                            className='max-md:backdrop-blur-md max-md:shadow-lg dark:max-md:bg-white/10 py-1 rounded-lg'
+                        >
+                            <Link className='block w-full' href='/'>
+                                <GlowingButton props={{
+                                    text: 'Home',
+                                    theme,
+                                    selectedPath: path === '/',
+                                }}
+                                />
+                            </Link>
+                        </motion.li>
+                        <motion.li
+                            variants={item}
+                            className='max-md:backdrop-blur-md max-md:shadow-lg  dark:max-md:bg-white/10 py-1 rounded-lg'
+                        >
+                            <Link className='block w-full' href='/about'>
+                                <GlowingButton props={{
+                                    text: 'About',
+                                    theme,
+                                    selectedPath: path === '/about'
+                                }}
+                                />
+                            </Link>
+                        </motion.li>
+                        <motion.li
+                            variants={item}
+                            className='max-md:backdrop-blur-md max-md:shadow-lg dark:max-md:bg-white/10 py-1 rounded-lg'
+                        >
+                            <Link className='block w-full' href='/projects'>
+                                <GlowingButton props={{
+                                    text: 'Projects',
+                                    theme,
+                                    selectedPath: path === '/projects'
+                                }}
+                                />
+                            </Link>
+                        </motion.li>
+                        <motion.li
+                            variants={item}
+                            className='max-md:backdrop-blur-md max-md:shadow-lg dark:max-md:bg-white/10 py-1 rounded-lg'
+                        >
+                            <Link className='block w-full' href='/contact'>
+                                <GlowingButton props={{
+                                    text: 'Contact',
+                                    theme,
+                                    selectedPath: path === '/contact'
+                                }}
+                                />
+                            </Link>
+                        </motion.li>
+                        <motion.li
+                            variants={item}
+                            className='md:ml-auto max-md:backdrop-blur-md max-md:shadow-lg dark:max-md:bg-white/10 py-1 rounded-lg'>
+                            <GlowingButton
+                                props={{
+                                    text: particles ? 'Disable Particles' : 'Enable Particles',
+                                    onClick: () => setParticles(!particles),
+                                    theme
+                                }} />
+                        </motion.li>
+                    </motion.div>
+                    <motion.div
+                        variants={container}
+                        initial='hide'
                         animate='show'
-                        className={`flex md:items-center ${isOpen ? 'max-md:mt-4 flex-col md:flex-row max-md:text-center' : 'max-md:hidden'} justify-center gap-2 z-50`}>
+                        className={`hidden md:flex md:items-center md:flex-row justify-center gap-2 z-50`}>
                         <motion.li
                             variants={item}
                             className='max-md:backdrop-blur-md max-md:shadow-lg dark:max-md:bg-white/10 py-1 rounded-lg'
