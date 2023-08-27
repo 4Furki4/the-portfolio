e378c9dd78dfc79ad24b7227e7d9cba8008373aa
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-08-27 21:22:49 +0300

    add: frontend-mentor-projects pics and data

--- db/static/projects.ts
+++ db/static/projects.ts
@@ -71,6 +71,58 @@ const projects: CardProps[] = [
         description: "The goal of this project is to improve upon TDK Dictionary (Official Online Turkish Dictionary) by adding new features such as client-side routing and enhancing the user interface.",
         repoLink: 'https://github.com/4Furki4/Turkish-Dictionary-Client',
         demoLink: 'https://turkish-dictionary-client.vercel.app/'
+    },
+    {
+        title: "Frontend Mentor Challenges",
+        images: [
+            {
+                src: '/pics/projects/frontend-mentor-challenges/testimonial-grid.png',
+                alt: 'Testimonial Grid Preview'
+            },
+            {
+                src: '/pics/projects/frontend-mentor-challenges/result-summary.png',
+                alt: 'Result Summary Preview'
+            },
+            {
+                src: '/pics/projects/frontend-mentor-challenges/clipboard-landing.png',
+                alt: 'Clipboard Landing Preview'
+            },
+            {
+                src: '/pics/projects/frontend-mentor-challenges/product-preview-card.png',
+                alt: 'Product Preview Card Preview'
+            },
+            {
+                src: '/pics/projects/frontend-mentor-challenges/signup-form-challenge.png',
+                alt: 'Signup Form Preview'
+            },
+            {
+                src: '/pics/projects/frontend-mentor-challenges/tip-calculator-challenge.png',
+                alt: 'Tip Calculator Preview'
+            },
+            {
+                src: '/pics/projects/frontend-mentor-challenges/interactive-rating-component.png',
+                alt: 'Interactive Rating Component Preview'
+            },
+            {
+                src: '/pics/projects/frontend-mentor-challenges/huddle-landing-page.png',
+                alt: 'Huddle Landing Page Preview'
+            },
+            {
+                src: '/pics/projects/frontend-mentor-challenges/age-calculator.png',
+                alt: 'Age Calculator Preview'
+            },
+            {
+                src: '/pics/projects/frontend-mentor-challenges/russian-circles.png',
+                alt: 'Russian Circles Preview'
+            },
+            {
+                src: '/pics/projects/frontend-mentor-challenges/dictionary-webapp.png',
+                alt: 'Dictionary Web App Preview'
+            },
+        ],
+        description: "Frontend Mentor is a platform that provides real-world challenges to help you improve your coding skills. I have completed 11 challenges so far.",
+        repoLink: 'https://github.com/4Furki4/Frontend-Projects/',
+        demoLink: '#'
     }
 ]
 
