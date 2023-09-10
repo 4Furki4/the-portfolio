c42d33468a44417e215cb817843fdf67a4255d37
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-09-10 12:45:49 +0300

    fix: missing metadata for projects page

--- app/(content)/projects/page.tsx
+++ app/(content)/projects/page.tsx
@@ -1,12 +1,19 @@
-import Card from '@/components/Card/Card'
-import React from 'react'
-import projects from '@/db/static/projects'
+import Card from "@/components/Card/Card";
+import React from "react";
+import projects from "@/db/static/projects";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Projects",
+  description: "Furkan Cengiz's projects with source code and live demos",
+};
+
 export default function Page() {
-    return (
-        <div className='z-50 p-2 pt-12 lg:w-10/12 mx-auto grid lg:grid-cols-2 gap-4'>
-            {projects.map((project, index) => (
-                <Card key={index} {...project} />
-            ))}
-        </div>
-    )
+  return (
+    <div className="z-50 p-2 pt-12 lg:w-10/12 mx-auto grid lg:grid-cols-2 gap-4">
+      {projects.map((project, index) => (
+        <Card key={index} {...project} />
+      ))}
+    </div>
+  );
 }
