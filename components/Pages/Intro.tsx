94cf14776b0dca8f6eece2f6962ce1e33b7b050a
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-09-11 15:16:02 +0300

    fix: intro header fs reduced

--- components/Pages/Intro.tsx
+++ components/Pages/Intro.tsx
@@ -21,23 +21,23 @@ export default function Intro() {
       initial="hidden"
       animate="show"
       variants={container}
-      className="h-full w-11/12 max-sm:mx-auto sm:w-full flex flex-col items-center justify-center text-center max-md:pt-[40%] mb-auto md:my-auto z-50 pointer-events-none"
+      className="h-full w-11/12 max-sm:mx-auto sm:w-full flex flex-col items-center justify-center text-center max-md:pt-[30%] mb-auto md:my-auto z-50 pointer-events-none"
     >
       <motion.h1
         variants={item}
-        className="text-fs-900 font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-red-700 to-purple-900 bg-300% animate-flow-gradient"
+        className="text-fs-700 lg:text-fs-900 font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-red-700 to-purple-900 bg-300% animate-flow-gradient"
       >
         {"Hello World, It's Furkan!"}
       </motion.h1>
       <motion.h2
         variants={item}
-        className="text-fs-600 font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-red-700 to-purple-900 bg-300% animate-flow-gradient my-6"
+        className="text-fs-500 font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-red-700 to-purple-900 bg-300% animate-flow-gradient my-6"
       >
         {"I'm a fullstack web developer."}
       </motion.h2>
       <motion.div
         variants={item}
-        className="h-[50px] grid grid-cols-3 sm:flex gap-2"
+        className="h-[50px] grid grid-cols-4 sm:flex gap-2"
       >
         <TechIcons />
       </motion.div>
