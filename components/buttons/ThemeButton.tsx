c58fecc302f4a9bd4903a633da4383835eb429c9
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-10-26 20:19:06 +0300

    fix: useEffect and isMounted state used to solve hydration error

--- components/buttons/ThemeButton.tsx
+++ components/buttons/ThemeButton.tsx
@@ -6,34 +6,38 @@ import { Moon, Sun } from "lucide-react";
 
 export default function ThemeButton() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = React.useState(false);
+  useEffect(() => setMounted(true), []);
   return (
-    <>
-      <div className="hidden md:block">
-        <Button
+    mounted && (
+      <>
+        <div suppressHydrationWarning className="hidden md:block">
+          <Button
+            type="button"
+            className="bg-transparent"
+            variant="flat"
+            onPress={() => setTheme(theme === "dark" ? "light" : "dark")}
+          >
+            {theme === "dark" ? (
+              <Sun aria-label="Sun Icon" size={32} />
+            ) : (
+              <Moon aria-label="Moon Icon" size={32} />
+            )}
+          </Button>
+        </div>
+        <button
+          aria-label="Theme Button"
           type="button"
-          className="bg-transparent"
-          variant="flat"
-          onPress={() => setTheme(theme === "dark" ? "light" : "dark")}
+          className="md:hidden"
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         >
           {theme === "dark" ? (
-            <Sun aria-label="Sun Icon" size={32} />
+            <Sun aria-label="Sun Icon" size={26} />
           ) : (
-            <Moon aria-label="Moon Icon" size={32} />
+            <Moon aria-label="Moon Icon" size={26} />
           )}
-        </Button>
-      </div>
-      <button
-        aria-label="Theme Button"
-        type="button"
-        className="md:hidden"
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      >
-        {theme === "dark" ? (
-          <Sun aria-label="Sun Icon" size={26} />
-        ) : (
-          <Moon aria-label="Moon Icon" size={26} />
-        )}
-      </button>
-    </>
+        </button>
+      </>
+    )
   );
 }
