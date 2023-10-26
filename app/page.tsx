ff73cb9529be9014d09d2f9794fa3f5d9accbdc6
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-10-26 21:47:49 +0300

    fix: lately added metadata added to page.tsx as well

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,34 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Furkan Cengiz",
   description: "Furkan Cengiz's personal website and portfolio",
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      follow: true,
+      index: true,
+    },
+  },
+  keywords: [
+    "Furkan Cengiz",
+    "Furki",
+    "Furki4_4",
+    "Developer",
+    "Software Engineer",
+    "Software Developer",
+    "Web Developer",
+    "Full Stack Developer",
+    "Frontend Developer",
+    "Backend Developer",
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://furki.vercel.app",
+    title: "Furkan Cengiz",
+    description: "Furkan Cengiz's personal website",
+  },
 };
 
 export default function Page() {
