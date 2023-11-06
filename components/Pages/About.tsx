f803f5d41522789e985ee3d37fc93f38519db2ad
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-11-06 07:49:44 +0300

    add: streaming with suspense first-try in github table

--- components/Pages/About.tsx
+++ components/Pages/About.tsx
@@ -1,16 +1,9 @@
-"use client";
 import Image from "next/image";
-import React from "react";
+import React, { Suspense } from "react";
 import { AboutMeParagraph } from "../About/AboutMeParagraph";
 import Education from "../About/Education";
 import GithubContributionTable from "../About/GithubContributionTable";
-export default function About({
-  blurredImage,
-  githubTableData,
-}: {
-  blurredImage: string;
-  githubTableData: GithubContributionResponse;
-}) {
+export default function About({ blurredImage }: { blurredImage: string }) {
   return (
     <section className="relative z-40 my-4 sm:my-16 mx-auto max-w-6xl p-6 backdrop-blur-xs grid gap-8">
       <figure>
@@ -28,7 +21,9 @@ export default function About({
       </figure>
       <AboutMeParagraph />
       <Education />
-      <GithubContributionTable githubTableData={githubTableData} />
+      <Suspense>
+        <GithubContributionTable />
+      </Suspense>
     </section>
   );
 }
