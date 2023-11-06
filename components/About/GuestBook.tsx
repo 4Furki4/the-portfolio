a246dc501c09b5fe7fd8420792ece65fca9b0cd0
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-11-06 20:32:10 +0300

    add: prisma and serverActions implemented, entries are rendered

--- components/About/GuestBook.tsx
+++ components/About/GuestBook.tsx
@@ -1,20 +1,54 @@
 import { getServerAuthSession } from "@/lib/auth";
-import React from "react";
+import React, { Suspense } from "react";
 import SigninForm from "./GuestBook/SigninForm";
 import GuestBookForm from "./GuestBook/GuestBookForm";
+import prisma from "@/db/db";
+import { Card, CardContent, CardHeader } from "../ui/card";
+import { formatRelative } from "date-fns";
 
 export default async function GuestBook() {
   const session = await getServerAuthSession();
   return (
-    <div>
-      {session ? (
-        <GuestBookForm />
-      ) : (
-        <div>
-          <h1>Please Login!</h1>
-          <SigninForm />
-        </div>
-      )}
-    </div>
+    <section className="flex flex-col gap-4">
+      {session ? <GuestBookForm /> : <SigninForm />}
+      <Suspense>
+        <GuestBookEntries />
+      </Suspense>
+    </section>
+  );
+}
+
+async function GuestBookEntries() {
+  const messages = await prisma.message.findMany({
+    take: 100,
+  });
+  return (
+    <Card
+      id="guestbook"
+      className="backdrop-blur-xs bg-opacity-60 dark:bg-opacity-60"
+    >
+      <CardHeader className="text-fs-500 text-center">Guest Book</CardHeader>
+      <CardContent>
+        {messages.length === 0 ? (
+          <p className="text-center text-fs-300 italic">
+            There are no messages yet.
+          </p>
+        ) : (
+          <ul>
+            {messages.map((message) => (
+              <li key={message.id} className="flex break-words">
+                <p>
+                  <span className="opacity-80">
+                    {formatRelative(message.createdAt, new Date())} -
+                  </span>{" "}
+                  <span className="font-bold">{message.name}:</span>{" "}
+                  {message.message}
+                </p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </CardContent>
+    </Card>
   );
 }
--- components/About/GuestBook/GuestBookForm.tsx
+++ components/About/GuestBook/GuestBookForm.tsx
@@ -1,12 +1,50 @@
 "use client";
+import addGuestEntry from "@/db/actions/addGuestEntry";
+import { Button, Input } from "@nextui-org/react";
 import { signOut } from "next-auth/react";
 import React from "react";
-
+//@ts-ignore Warning: useFormStatus is now in canary. Remove the experimental_ prefix. The prefixed alias will be removed in an upcoming release.
+import { FormStatus, useFormStatus } from "react-dom";
 export default function GuestBookForm() {
+  const formRef = React.useRef<HTMLFormElement>(null);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const { pending }: FormStatus = useFormStatus();
   return (
-    <div>
-      <form action=""></form>
-      <button onClick={() => signOut()}>Signout</button>
+    <div className="flex flex-col gap-4">
+      <form
+        ref={formRef}
+        className="flex gap-2"
+        action={async (formData) => {
+          await addGuestEntry(formData);
+          formRef.current?.reset();
+        }}
+      >
+        <Input
+          ref={inputRef}
+          name="entry"
+          variant="underlined"
+          type="text"
+          isRequired
+          isDisabled={pending}
+          className="w-full"
+          aria-label="Leave your message..."
+          placeholder="Leave your message.."
+        />
+        <Button isLoading={pending} type="submit">
+          Submit
+        </Button>
+      </form>
+      <Button
+        className="hover:bg-background"
+        variant="ghost"
+        onPress={() =>
+          signOut({
+            callbackUrl: "/about#guestbook",
+          })
+        }
+      >
+        Sign out
+      </Button>
     </div>
   );
 }
