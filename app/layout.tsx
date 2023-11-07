8d1a8c8d5c7a236d45b1fd2671ec5b13bd5d7268
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-11-07 19:25:42 +0300

    ref: vh replaced with dvh

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`w-full transition-colors duration-300 text-foreground bg-background relative min-h-screen flex flex-col`}
+        className={`w-full transition-colors duration-300 text-foreground bg-background relative min-h-dscreen flex flex-col`}
       >
         <Providers>
           <Navbar />
@@ -70,7 +70,7 @@ export default function RootLayout({
           {
             <div
               aria-hidden
-              className="w-full min-h-screen absolute top-0 left-0"
+              className="w-full min-h-dscreen absolute top-0 left-0"
             >
               <BackgroundParticles />
             </div>
