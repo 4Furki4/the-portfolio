e99ec1b26efd3e89708467f21d11a4f1c3d18a55
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-11-10 17:13:45 +0300

    fix: tsparticles detectOn interactivity changed to window so, even though an element is on top of the particles, we can still interact with the particles

--- components/particles/BackgroundParticles.tsx
+++ components/particles/BackgroundParticles.tsx
@@ -1,11 +1,12 @@
-"use client"
+"use client";
 import React, { useCallback } from "react";
 import Particles from "react-particles";
 import particlesConfig from "./config";
 import { loadSlim } from "tsparticles-slim";
 import { useParticleContext } from "@/context/ParticleContext";
+import { Container, Engine } from "tsparticles-engine";
 export default function BackgroundParticles() {
-  const particlesInit = useCallback(async (engine) => {
+  const particlesInit = useCallback(async (engine: Engine) => {
     console.log(engine);
     // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
@@ -14,20 +15,20 @@ export default function BackgroundParticles() {
     await loadSlim(engine);
   }, []);
 
-  const particlesLoaded = useCallback(async (container) => {
+  const particlesLoaded = useCallback(async (container?: Container) => {
     await console.log(container);
   }, []);
-  const {particles, handleParticles} = useParticleContext()
+  const { particles, handleParticles } = useParticleContext();
 
   return (
     particles && (
       <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      style={{ "minHeight": "100dvh", "width": "100%" }}
-      options={particlesConfig}
-    />
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        style={{ minHeight: "100dvh", width: "100%" }}
+        options={particlesConfig}
+      />
     )
   );
 }
