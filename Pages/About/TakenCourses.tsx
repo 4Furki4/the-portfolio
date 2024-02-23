abc624c9f531e1933b137c2aac9470f2d49aa8bb
Furkan Cengiz <muhammedcengiz1@gmail.com>
2024-02-23 09:03:58 +0300

    Refactor TakenCourses component layout

--- Pages/About/TakenCourses.tsx
+++ Pages/About/TakenCourses.tsx
@@ -11,18 +11,20 @@ export default function TakenCourses() {
             </CardHeader>
             <CardContent className='grid place-content-center md:grid-cols-2 gap-4'>
                 {takenCourses.map((takenCourse) => (
-                    <Card key={takenCourse.courseTitle} className='max-w-md'>
+                    <Card key={takenCourse.courseTitle} className='flex flex-col'>
                         <CardHeader className='text-center text-fs-400'>
                             {takenCourse.courseTitle}
                         </CardHeader>
-                        <CardContent>
+                        <CardContent className='space-y-4'>
                             <Image className='text-center' src={takenCourse.image} alt={takenCourse.courseTitle} width={450} height={400} sizes={
                                 '(max-width: 450px) 100vw, 450px'
                             } />
-                            <p>{takenCourse.summarize}</p>
-                            <Link target='_blank' href={takenCourse.courseLink}>Course Page</Link>
+                            <div>
+                                <p>{takenCourse.summarize}</p>
+                                <Link target='_blank' href={takenCourse.courseLink}>Course Page</Link>
+                            </div>
                         </CardContent>
-                        <CardFooter className='flex flex-wrap gap-2'>
+                        <CardFooter className='flex flex-wrap gap-2 mt-auto'>
                             {takenCourse.tags.map((tag, index) => (
                                 <p key={index} className='text-fs-200 bg-foreground-foreground rounded-full px-3 py-1'>
                                     {tag}
